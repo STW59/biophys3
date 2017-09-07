aa783c36e50a0f85088b1259b29156dbc691348d
Stephen E. White <stw59@pitt.edu>
2017-09-07 14:00:31 -0400

    Homework 1: Functionally complete

--- Homework1/PDB_Converter.py
+++ Homework1/PDB_Converter.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plot
 
 FILENAME = "1qcq.pdb"
 TABLE1_OUTPUT = "1qcq_t1.txt"
@@ -51,7 +52,7 @@ def main():
     for line in pdb_file:
         if line[0:4] == "ATOM" and line[13:15] in ("N ", "CA", "C "):
             index = int(line[6:11])
-            atom = str(line[12:16])  # TODO: Remove spaces from atom string
+            atom = str(line[13:15])  # TODO: Remove spaces from atom string
             residue = str(line[17:20])
             atom_loc = np.array([float(line[30:38]),
                                  float(line[38:46]),
@@ -91,20 +92,20 @@ def main():
         theta = calc_bond_angle(atom0[3], atom1[3], atom2[3])
         angles.append([atom1[0], theta])
 
-    # t1 = open(TABLE1_OUTPUT, 'w')
-    # t1.write('Table 1: Bond Length and Supplemental Bond Angle Theta\n')
-    # t1.write('Index,Atom,Bond_Length,Theta\n')
-    # for n in range(0, len(bonds)):
-    #     t1.write(str(bonds[n][0]) + ',' +
-    #              str(all_atoms[n][1]) + ',' +
-    #              str(bonds[n][1]) + ',' +
-    #              str(angles[n][1]) + '\n')
-
-    # TODO: Make the table columns a consistent width using spaces
+    t1 = open(TABLE1_OUTPUT, 'w')
+    t1.write('Table 1: Bond Length and Supplemental Bond Angle Theta\n')
+    t1.write('Index,Atom,Bond_Length,Theta\n')
+    for n in range(0, len(bonds)):
+        t1.write(str(bonds[n][0]) + ',' +
+                 str(all_atoms[n][1]) + ',' +
+                 str(bonds[n][1]) + ',' +
+                 str(angles[n][1]) + '\n')
 
     # Build Table 2
+    dihedrals = []
+
     # Calc Phi, Psi, Omega
-    print('phi psi omega')
+    index = 1
     for n in range(1, len(all_atoms), 3):
         try:
             atom0 = all_atoms[n - 2]
@@ -120,8 +121,8 @@ def main():
             phi = 'N/A'
             psi = calc_dihedral(atom1[3], atom2[3], atom3[3], atom4[3])
             omega = calc_dihedral(atom2[3], atom3[3], atom4[3], atom5[3])
-        elif n == len(all_atoms):
-            phi = calc_dihedral(atom2[3], atom3[3], atom4[3], atom5[3])
+        elif n == len(all_atoms) - 2:
+            phi = calc_dihedral(atom0[3], atom1[3], atom2[3], atom3[3])
             psi = 'N/A'
             omega = 'N/A'
         else:
@@ -129,9 +130,39 @@ def main():
             psi = calc_dihedral(atom1[3], atom2[3], atom3[3], atom4[3])
             omega = calc_dihedral(atom2[3], atom3[3], atom4[3], atom5[3])
 
-        print(phi, psi, omega)
+        dihedrals.append([index, atom2[2], phi, psi, omega])
+        index += 1
 
-# Table 1: Index, Atom, Bond Length, Bond Angle (theta)
 # Table 2: Index, Residue, Phi, Psi, Omega
+    t2 = open(TABLE2_OUTPUT, 'w')
+    t2.write('Table 2: The Three Dihedrals\n')
+    t2.write('Index,Residue,Phi,Psi,Omega\n')
+    for residue in dihedrals:
+        t2.write(str(residue[0]) + ',' +
+                 str(residue[1]) + ',' +
+                 str(residue[2]) + ',' +
+                 str(residue[3]) + ',' +
+                 str(residue[4]) + '\n')
+
+    # TODO: Make Ramachandran plot
+    phi_plot = []
+    psi_plot = []
+    for residue in dihedrals:
+        if 'N/A' not in residue:
+            phi_plot.append(residue[2])
+            psi_plot.append(residue[3])
+
+    fig, ax = plot.subplots()
+    ax.scatter(phi_plot, psi_plot)
+    ax.set_xlabel(r'$\Phi$ [Degrees]', fontsize=12)
+    ax.set_ylabel(r'$\Psi$ [Degrees]', fontsize=12)
+    ax.set_title('Ramachandran Plot', fontsize=16)
+    ax.set_autoscale_on(False)
+    ax.axis([-180, 180, -180, 180])
+
+    ax.grid(True)
+    fig.tight_layout()
+
+    plot.show()
 
 main()
