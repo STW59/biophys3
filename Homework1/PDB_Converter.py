5c723948ecf795e6315effe37c1d4aaae76cc79e
Stephen E. White <stw59@pitt.edu>
2017-09-07 15:24:56 -0400

    Homework 1: Completed formatting, added non-test pdb file

--- Homework1/PDB_Converter.py
+++ Homework1/PDB_Converter.py
@@ -1,9 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plot
 
-FILENAME = "1qcq.pdb"
-TABLE1_OUTPUT = "1qcq_t1.txt"
-TABLE2_OUTPUT = "1qcq_t2.txt"
+"""
+1RUZ: 1918 H1 Hemagglutinin
+Paper DOI: 10.1126/science.1093155
+"""
+
+FILENAME = "1ruz.pdb"
+TABLE1_OUTPUT = "1ruz_t1.txt"
+TABLE2_OUTPUT = "1ruz_t2.txt"
 
 
 def calc_bond_length(r0, r1):
@@ -28,6 +33,14 @@ def calc_bond_angle(r1, r2, r3):
 
 
 def calc_dihedral(r1, r2, r3, r4):
+    """
+    Calculates the dihedral angle between four position vectors
+    :param r1: position of first atom
+    :param r2: position of second atom
+    :param r3: position of third atom
+    :param r4: position of fourth atom
+    :return: returns dihedral angle
+    """
     l1 = r2 - r1
     l2 = r3 - r2
     l3 = r4 - r3
@@ -52,7 +65,9 @@ def main():
     for line in pdb_file:
         if line[0:4] == "ATOM" and line[13:15] in ("N ", "CA", "C "):
             index = int(line[6:11])
-            atom = str(line[13:15])  # TODO: Remove spaces from atom string
+            atom = str(line[13:15])
+            if ' ' in atom:
+                atom = atom.replace(' ', '')
             residue = str(line[17:20])
             atom_loc = np.array([float(line[30:38]),
                                  float(line[38:46]),
@@ -76,7 +91,7 @@ def main():
             pass
 
         bond_length = calc_bond_length(atom0[3], atom1[3])
-        bonds.append([atom1[0], bond_length])
+        bonds.append([atom1[0], round(bond_length, 3)])
 
     # Theta Calculation
     angles.append([1, 'N/A'])
@@ -90,16 +105,17 @@ def main():
             break
 
         theta = calc_bond_angle(atom0[3], atom1[3], atom2[3])
-        angles.append([atom1[0], theta])
+        angles.append([atom1[0], np.around(theta, 3)])
 
     t1 = open(TABLE1_OUTPUT, 'w')
     t1.write('Table 1: Bond Length and Supplemental Bond Angle Theta\n')
-    t1.write('Index,Atom,Bond_Length,Theta\n')
+    t1.write('Index  Atom  Bond Length  Theta\n')
     for n in range(0, len(bonds)):
-        t1.write(str(bonds[n][0]) + ',' +
-                 str(all_atoms[n][1]) + ',' +
-                 str(bonds[n][1]) + ',' +
-                 str(angles[n][1]) + '\n')
+        col_index = str(bonds[n][0]) + '{}'.format(' ' * (7 - len(str(bonds[n][0]))))
+        col_atom = str(all_atoms[n][1]) + '{}'.format(' ' * (6 - len(str(all_atoms[n][1]))))
+        col_bond_length = str(bonds[n][1]) + '{}'.format(' ' * (13 - len(str(bonds[n][1]))))
+        col_theta = str(angles[n][1])
+        t1.write(str(col_index) + str(col_atom) + str(col_bond_length) + str(col_theta) + '\n')
 
     # Build Table 2
     dihedrals = []
@@ -119,16 +135,16 @@ def main():
 
         if n == 1:
             phi = 'N/A'
-            psi = calc_dihedral(atom1[3], atom2[3], atom3[3], atom4[3])
-            omega = calc_dihedral(atom2[3], atom3[3], atom4[3], atom5[3])
+            psi = np.around(calc_dihedral(atom1[3], atom2[3], atom3[3], atom4[3]), 3)
+            omega = np.around(calc_dihedral(atom2[3], atom3[3], atom4[3], atom5[3]), 3)
         elif n == len(all_atoms) - 2:
-            phi = calc_dihedral(atom0[3], atom1[3], atom2[3], atom3[3])
+            phi = np.around(calc_dihedral(atom0[3], atom1[3], atom2[3], atom3[3]), 3)
             psi = 'N/A'
             omega = 'N/A'
         else:
-            phi = calc_dihedral(atom0[3], atom1[3], atom2[3], atom3[3])
-            psi = calc_dihedral(atom1[3], atom2[3], atom3[3], atom4[3])
-            omega = calc_dihedral(atom2[3], atom3[3], atom4[3], atom5[3])
+            phi = np.around(calc_dihedral(atom0[3], atom1[3], atom2[3], atom3[3]), 3)
+            psi = np.around(calc_dihedral(atom1[3], atom2[3], atom3[3], atom4[3]), 3)
+            omega = np.around(calc_dihedral(atom2[3], atom3[3], atom4[3], atom5[3]), 3)
 
         dihedrals.append([index, atom2[2], phi, psi, omega])
         index += 1
@@ -136,15 +152,15 @@ def main():
 # Table 2: Index, Residue, Phi, Psi, Omega
     t2 = open(TABLE2_OUTPUT, 'w')
     t2.write('Table 2: The Three Dihedrals\n')
-    t2.write('Index,Residue,Phi,Psi,Omega\n')
+    t2.write('Index  Residue  Phi       Psi       Omega\n')
     for residue in dihedrals:
-        t2.write(str(residue[0]) + ',' +
-                 str(residue[1]) + ',' +
-                 str(residue[2]) + ',' +
-                 str(residue[3]) + ',' +
-                 str(residue[4]) + '\n')
+        col_index = str(residue[0]) + '{}'.format(' ' * (7 - len(str(residue[0]))))
+        col_residue = str(residue[1]) + '{}'.format(' ' * (9 - len(str(residue[1]))))
+        col_phi = str(residue[2]) + '{}'.format(' ' * (10 - len(str(residue[2]))))
+        col_psi = str(residue[3]) + '{}'.format(' ' * (10 - len(str(residue[3]))))
+        col_omega = str(residue[4])
+        t2.write(col_index + col_residue + col_phi + col_psi + col_omega + '\n')
 
-    # TODO: Make Ramachandran plot
     phi_plot = []
     psi_plot = []
     for residue in dihedrals:
@@ -164,5 +180,6 @@ def main():
     fig.tight_layout()
 
     plot.show()
+
 
 main()
